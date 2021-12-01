8e6453bc9a3beec99bc53813beb971286ef09f00
Vishal Rathod <vrathod591@gmail.com>
2021-12-02 01:07:38 +0530

    Updated Navbar

--- portfolio/src/Components/Navbar/Toggle.jsx
+++ portfolio/src/Components/Navbar/Toggle.jsx
@@ -8,21 +8,20 @@ export const Toggle = ({ themeToggler }) => {
     themeToggler();
   };
   return (
-    <div style={{border:"1px solid #021f34",borderRadius:"20px"}}>
-    <Switch
-      handleDiameter={22}
-      width={65}
-      height={37}
-      checked={isDark}
-      onChange={handleChange}
-      checkedIcon={
-        <div style={{padding:"3px"}}>🌙</div>
-      }
-      uncheckedIcon={<div style={{padding:"3px"}}>🌞</div>}
-      onColor="#fff"
-      offColor="#fff"
-      offHandleColor="#0556f3"
-      onHandleColor="#00AAF2"
-    /></div>
+    <div style={{ border: "1px solid #021f34", borderRadius: "20px" }}>
+      <Switch
+        handleDiameter={22}
+        width={65}
+        height={37}
+        checked={isDark}
+        onChange={handleChange}
+        checkedIcon={<div style={{ padding: "3px" }}>🌙</div>}
+        uncheckedIcon={<div style={{ padding: "3px" }}>🌞</div>}
+        onColor="#fff"
+        offColor="#fff"
+        offHandleColor="#0556f3"
+        onHandleColor="#00AAF2"
+      />
+    </div>
   );
 };
