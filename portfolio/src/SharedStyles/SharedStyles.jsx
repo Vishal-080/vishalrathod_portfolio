f655e593db59711ab9112342ad9cfdf2b53fb5ea
Vishal Rathod <vrathod591@gmail.com>
2021-12-02 01:13:49 +0530

    Updated Projects section details

--- portfolio/src/SharedStyles/SharedStyles.jsx
+++ portfolio/src/SharedStyles/SharedStyles.jsx
@@ -1,7 +1,6 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-
   width: 100%;
   padding: 40px 0px;
   /* margin-bottom: 100px; */
